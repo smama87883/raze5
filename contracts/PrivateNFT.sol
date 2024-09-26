92ecb74de7fe4fa08624d50dfc45bde5dd9421a8
smama 87883 <smama87883@gmail.com>
2024-09-26 14:08:32 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("hhh","jjj")
+        ERC721("lll","ooo")
         Ownable(initialOwner)
     {}
 
